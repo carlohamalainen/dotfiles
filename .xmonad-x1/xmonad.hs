0b89df77420573933882578e17d11c4a757fc6a2
Carlo Hamalainen <carlo.hamalainen@gmail.com>
2013-07-03 07:23:43 +1000

    xmonadrc

---
+++ .xmonad-x1/xmonad.hs
@@ -0,0 +1,15 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+
+main = do
+    spawn "mate-session"
+    xmonad $ defaultConfig
+        { manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        } `additionalKeys`
+        [ ((controlMask, xK_F11), spawn "mate-terminal")
+        , ((controlMask, xK_F12), spawn "firefox") ]
