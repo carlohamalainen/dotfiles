e8dd10b5eef6d64a1a3b357bdf35b2c0aba50d07
Carlo Hamalainen <carlo.hamalainen@gmail.com>
2013-11-02 16:43:06 +0100

    Kernel config for X1 Carbon using 3.11.6 kernel.

--- .xmonad-x1/xmonad.hs
+++ .xmonad-x1/xmonad.hs
@@ -5,11 +5,33 @@ import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
+{-
 main = do
-    spawn "mate-session"
+    spawn "gnome-session"
     xmonad $ defaultConfig
         { manageHook = manageDocks <+> manageHook defaultConfig
         , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        -- , modMask = mod4Mask
         } `additionalKeys`
-        [ ((controlMask, xK_F11), spawn "mate-terminal")
-        , ((controlMask, xK_F12), spawn "/opt/firefox/firefox") ]
+        [ ((controlMask, xK_F10), spawn "/home/carlo/opt/thunderbird-24.1.0/thunderbird")
+        , ((controlMask, xK_F11), spawn "gnome-terminal")
+        , ((controlMask, xK_F12), spawn "/usr/bin/google-chrome")
+        ]
+-}
+
+main = do
+    xmproc <- spawnPipe "/home/carlo/.cabal/bin/xmobar /home/carlo/.xmobarrc"
+    xmonad $ defaultConfig
+        { manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , logHook = dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        }
+        -- , modMask = mod4Mask     -- Rebind Mod to the Windows key
+        } `additionalKeys`
+        [ ((controlMask, xK_F9), spawn "nautilus")
+        , ((controlMask, xK_F10), spawn "/home/carlo/opt/thunderbird-24.1.0/thunderbird")
+        , ((controlMask, xK_F11), spawn "gnome-terminal")
+        , ((controlMask, xK_F12), spawn "/usr/bin/google-chrome")
+        ]
