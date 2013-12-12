ec95ec8628ec46602793e0e6cd38b130942423eb
Carlo Hamalainen <carlo.hamalainen@gmail.com>
2013-12-12 13:00:07 +0100

    Different video codec, notes on editing with avidemux.

--- bin/record_screencast.sh
+++ bin/record_screencast.sh
@@ -2,7 +2,15 @@
 
 if [ $# -gt 0 ]; then
     file=$1
-    ffmpeg -f alsa -i default  -f x11grab -show_region 1 -s cif -r 25 -s 1280x720 -i :0.0+100,100 -acodec pcm_s16le -vcodec libx264 -preset ultrafast -crf 0 -threads 0 $file
+    # ffmpeg -f alsa -i default  -f x11grab -show_region 1 -s cif -r 25 -s 1280x720 -i :0.0+100,100 -acodec pcm_s16le -vcodec libx264 -preset ultrafast -crf 0 -threads 0 $file
+
+    # https://wiki.ubuntu.com/ScreenCasts/ffmpeg
+    ffmpeg -f alsa -i default  -f x11grab -show_region 1 -s cif -r 25 -s 1280x720 -i :0.0+100,100 -acodec libmp3lame -ab 128k -vcodec huffyuv $file
+
+    # Note to self: record using this huffyuv lossless codec. For editing,
+    # open in avidemux, use [ and ] to select a region, then cut/delete/paste/etc.
+    # Finally, export to MP4 with AAC audio. Seems to play ok in mplayer and vlc.
+
 else
     echo
     echo "Handy wmctrl command to resize and relocate the terminal windows:"
