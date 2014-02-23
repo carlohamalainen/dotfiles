f8c75fd99e83bc464ba192699c8ad5990bfca4b9
Carlo Hamalainen <carlo@carlo-hamalainen.net>
2014-02-23 16:00:10 +0100

    Use gnome-session instead of mate-session.

--- bin/tray_and_gnome_session.sh
+++ bin/tray_and_gnome_session.sh
@@ -1,2 +1,3 @@
 trayer --edge top --align right --SetDockType true --SetPartialStrut true  --expand true --width 10 --transparent true --tint 0x191970 --height 20 &
 gnome-session &
+# mate-session &
